5bdff1395e6c45751586c2032a9d3629335fa27c
EvelynCriste <161662453+EvelynCriste@users.noreply.github.com>
2024-06-05 15:49:21 -0300

    Add files via upload

---
+++ prova03__2DSN/Apresentação/FrmFornecedor.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace Apresentação
+{
+    partial class FrmFornecedor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            dataGridView1 = new DataGridView();
+            listBox1 = new ListBox();
+            listBox2 = new ListBox();
+            listBox3 = new ListBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            radioButton1 = new RadioButton();
+            radioButton2 = new RadioButton();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(12, 420);
+            button1.Name = "button1";
+            button1.Size = new Size(101, 39);
+            button1.TabIndex = 0;
+            button1.Text = "Novo";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(132, 420);
+            button2.Name = "button2";
+            button2.Size = new Size(101, 39);
+            button2.TabIndex = 1;
+            button2.Text = "Alterar";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(248, 420);
+            button3.Name = "button3";
+            button3.Size = new Size(101, 39);
+            button3.TabIndex = 2;
+            button3.Text = "Excluir";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(374, 420);
+            button4.Name = "button4";
+            button4.Size = new Size(101, 39);
+            button4.TabIndex = 3;
+            button4.Text = "Salvar";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(569, 420);
+            button5.Name = "button5";
+            button5.Size = new Size(101, 39);
+            button5.TabIndex = 4;
+            button5.Text = "Cancelar";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(12, 220);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(647, 180);
+            dataGridView1.TabIndex = 5;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(12, 30);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(81, 34);
+            listBox1.TabIndex = 6;
+            // 
+            // listBox2
+            // 
+            listBox2.FormattingEnabled = true;
+            listBox2.ItemHeight = 15;
+            listBox2.Location = new Point(12, 100);
+            listBox2.Name = "listBox2";
+            listBox2.Size = new Size(387, 34);
+            listBox2.TabIndex = 7;
+            // 
+            // listBox3
+            // 
+            listBox3.FormattingEnabled = true;
+            listBox3.ItemHeight = 15;
+            listBox3.Location = new Point(12, 180);
+            listBox3.Name = "listBox3";
+            listBox3.Size = new Size(387, 34);
+            listBox3.TabIndex = 8;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 12);
+            label1.Name = "label1";
+            label1.Size = new Size(20, 15);
+            label1.TabIndex = 9;
+            label1.Text = "Id:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 82);
+            label2.Name = "label2";
+            label2.Size = new Size(43, 15);
+            label2.TabIndex = 10;
+            label2.Text = "Nome:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(12, 162);
+            label3.Name = "label3";
+            label3.Size = new Size(39, 15);
+            label3.TabIndex = 11;
+            label3.Text = "Email:";
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(193, 30);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(93, 19);
+            radioButton1.TabIndex = 12;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "Pessoa Fisica";
+            radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(193, 55);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(104, 19);
+            radioButton2.TabIndex = 13;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "Pessoa Juridica";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // FrmFornecedor
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 489);
+            Controls.Add(radioButton2);
+            Controls.Add(radioButton1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(listBox3);
+            Controls.Add(listBox2);
+            Controls.Add(listBox1);
+            Controls.Add(dataGridView1);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "FrmFornecedor";
+            Text = "FrmFornecedor";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private DataGridView dataGridView1;
+        private ListBox listBox1;
+        private ListBox listBox2;
+        private ListBox listBox3;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private RadioButton radioButton1;
+        private RadioButton radioButton2;
+    }
+}
---
+++ prova03__2DSN/Apresentação/FrmPrompt.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Apresentação
+{
+    partial class FrmPrompt
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            listBox1 = new ListBox();
+            button1 = new Button();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(12, 47);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(527, 49);
+            listBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(555, 37);
+            button1.Name = "button1";
+            button1.Size = new Size(161, 59);
+            button1.TabIndex = 1;
+            button1.Text = "Enviar";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 18);
+            label1.Name = "label1";
+            label1.Size = new Size(69, 15);
+            label1.TabIndex = 2;
+            label1.Text = "Digite Algo:";
+            label1.Click += label1_Click;
+            // 
+            // FrmPrompt
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaption;
+            ClientSize = new Size(746, 118);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Controls.Add(listBox1);
+            Name = "FrmPrompt";
+            Text = "FrmPrompt";
+            Load += FrmPrompt_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ListBox listBox1;
+        private Button button1;
+        private Label label1;
+    }
+}
